0415670e705eb0af9fdacd0258572b5f2491fc2f
SriRamMadhav <mukkamalasrirammadhav@gmail.com>
2025-07-19 11:25:57 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,21 @@
+public class datatype {
+	public static void main(String args[]) {
+	char a = 'G';
+	int i = 38;
+	byte b = 4;
+	short s = 56;
+	double d = 4.2376293693;
+	float f = 4.374936f;
+	long l = 234567;
+	System.out.println("char: "+a);
+	System.out.println("int: "+i);
+	System.out.println("byte: "+b);
+	System.out.println("short: "+s);
+	System.out.println("double: "+d);
+	System.out.println("float: "+f);
+	System.out.println("long: "+l);
+	
+	
+	
+	}
+}
